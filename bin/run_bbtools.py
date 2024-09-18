fad273593f74c90cabf1a4e12b8a6edb0f62dba3
Pete Dockrill <zcempjd@ucl.ac.uk>
2024-09-18 15:09:40 +0100

    BBTools: basic wrapper script and process added, as well as docker cconfigs

---
+++ bin/run_bbtools.py
@@ -0,0 +1,28 @@
+## run_bbtools.py - Python wrapper for BBToolsRunner (see https://gitlab.com/ezlab/busco/-/blob/master/src/busco/busco_tools/bbtools.py?ref_type=heads) 
+
+from busco.busco_tools.bbtools import BBToolsRunner
+import argparse
+
+def run_bbtools(input_file:str, output_file:str):
+
+    bbtools_runner = BBToolsRunner()
+
+    bbtools_runner.input_file = input_file
+    bbtools_runner.output_file = output_file
+
+    bbtools_runner.configure_runner()
+
+    bbtools_runner.run()
+
+    bbtools_runner.parse_output()
+
+    print(bbtools_runner.metrics, file = bbtools_runner.output_file)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--input', nargs =1, type=str)
+    parser.add_argument('-o', '--output', nargs =1, type=str)
+
+    run_bbtools()
---
+++ modules/local/ncbidatasets/run_bbtools.nf
@@ -0,0 +1,16 @@
+process runBBtools {
+    container = ?
+
+    input:
+    path input_file 
+    path output_folder 
+
+    output: 
+    path "${output_folder}/bbtools_output"
+
+    script:
+    """
+    python bin/run_bbtools.py $input_file $output_folder
+    """
+
+}
